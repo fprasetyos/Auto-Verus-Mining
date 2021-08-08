179c529cf585921b2c14938be6a882ceab4d0fac
fprasetyos <88612934+fprasetyos@users.noreply.github.com>
2021-08-08 19:38:33 +0700

    Update autominer.sh

--- autominer.sh
+++ autominer.sh
@@ -1,1 +1,1 @@
-/root/ccminer/ccminer -a verus -o stratum+tcp://ap.luckpool.net:3960 -u RQJR88qRa79JjkeUqj1uRh88VHMukstV5c.STB2 -p x -t 2
+/root/ccminer/ccminer -a verus -o stratum+tcp://ap.luckpool.net:3960 -u RQJR88qRa79JjkeUqj1uRh88VHMukstV5c.$(hostname) -p x -t 2
